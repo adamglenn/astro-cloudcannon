e34a8886c49e78be73230fdc74ee014754ac6a00
atglenn <adam.glenn@gmail.com>
2023-09-19 18:34:18 -0400

    Adding dynamic components.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,23 +4,23 @@ const pages = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    heroComponent: z.object({
-      header: z.object({
-        text: z.string(),
-        style: z.string(),
-      }),
-      subhead: z.string(),
-      content: z.string(),
-      image: z.object({
-        src: z.string(),
-        alt: z.string(),
-      }),
-      button: z.object({
-        text: z.string(),
-        style: z.string(),
-        href: z.string(),
-      }),
-    }),
+    // heroComponent: z.object({
+    //   header: z.object({
+    //     text: z.string(),
+    //     style: z.string(),
+    //   }),
+    //   subhead: z.string(),
+    //   content: z.string(),
+    //   image: z.object({
+    //     src: z.string(),
+    //     alt: z.string(),
+    //   }),
+    //   button: z.object({
+    //     text: z.string(),
+    //     style: z.string(),
+    //     href: z.string(),
+    //   }),
+    // }),
   })
 })
 
--- src/content/pages/index.md
+++ src/content/pages/index.md
@@ -1,4 +1,6 @@
 ---
 layout: ../../layouts/PageLayout.astro
+title: My Index Title
+description: My Index Description
 content_blocks:
 ---
